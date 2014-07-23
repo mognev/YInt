65d8068557857f1c81958c628822715a1ff46f59
mognev <mognev@ruswizards.com>
2014-07-23 07:45:28 +0400

    Merged
    Signed-off-by: mognev <mognev@ruswizards.com>

--- Core/Configuration/Constant/DefaultConfigarationConstant.cs
+++ Core/Configuration/Constant/DefaultConfigarationConstant.cs
@@ -10,8 +10,6 @@ namespace Core.Configuration.Constant
     {
         static DefaultConfigarationConstant()
         {
-            Clid = "19567833";
-            ApiKey = "9a90ac5f1419459a99f9804f0255c6b4";
             CancelSchedule = false;
             DebugMode = false;
             DebugInformation = "DebugInformation";
@@ -19,8 +17,6 @@ namespace Core.Configuration.Constant
             StopSpeed = 5;
         }
 
-        public static String Clid { get; private set; }
-        public static String ApiKey { get; private set; }
         public static Boolean CancelSchedule { get; private set; }
         public static Boolean DebugMode { get; private set; }
         public static String DebugInformation { get; private set; }
